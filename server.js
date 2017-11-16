c9d36a1693b1ac3b44bd3e2445406a84e8b87855
Heather Rae <heatherrae150@yahoo.co.uk>
2017-11-16 23:57:11 +0000

    heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static('client/build'))
 // allows cross origin resource sharing
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header("Access-Control-Allow-Origin", "https://react-battleships.herokuapp.com/")
+  res.header("Access-Control-Allow-Origin", "https://git.heroku.com/react-battleships.git")
   res.header("Access-Control-Allow-Credentials", true)
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
